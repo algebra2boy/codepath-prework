62a7f5a3dee7d1e5ff565854f2c3c06b0c377d39
Yongye <103079472+algebra2boy@users.noreply.github.com>
2022-07-22 21:27:59 -0400

    Added prework to repo

---
+++ Prework/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Hugo‘s system on 7/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ChangingText(_ sender: Any) {
+        let mtext = TextField.text
+        TextLabel.text = mtext
+        TextField.text = ""
+    }
+    
+    
+    @IBAction func BackgroundColorChange(_ sender: Any) {
+
+        view.backgroundColor = UIColor.brown
+    }
+    @IBAction func ButtonClicked(_ sender: Any) {
+        TextLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func GoodBye(_ sender: Any) {
+        TextLabel.text = "GoodBye, I will miss you!"
+    }
+
+    
+}
